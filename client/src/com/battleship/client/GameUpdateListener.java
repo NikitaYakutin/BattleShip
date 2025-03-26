28fc515ce6c4433570bfedab69eee6f3e8c083e9
NikitaYakutin <146551469+NikitaYakutin@users.noreply.github.com>
2025-03-26 12:13:27 +0200

    Delete client/src/com/battleship/client/GameUpdateListener.java

--- client/src/com/battleship/client/GameUpdateListener.java
+++
@@ -1,8 +0,0 @@
-package com.battleship.client;
-
-import com.battleship.common.MoveResult;
-
-public interface GameUpdateListener {
-    void updateGame(MoveResult result);
-    void updateStatus(String status);
-}
